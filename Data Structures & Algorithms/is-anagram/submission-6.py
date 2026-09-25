6cce5ba1225b7a43c5ebf44f2810cdfc05a573a1
Fardin <fardin.shakir7@gmail.com>
2026-09-25 15:40:27 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for letter in s:
+            count[letter] = count.get(letter, 0) + 1
+
+        for letter in t:
+            if letter not in count:
+                return False
+
+            count[letter] -= 1
+
+        return not any(count.values())
